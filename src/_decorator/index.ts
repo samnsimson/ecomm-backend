7119c3464aeb6de4cc7b94d054841226fcb4e010
samnsimson <samnishanth01@gmail.com>
2024-05-15 22:22:51 -0400

    current user decorator

--- src/_decorator/index.ts
+++ src/_decorator/index.ts
@@ -1,4 +1,10 @@
-import { SetMetadata } from '@nestjs/common';
+import { ExecutionContext, SetMetadata, createParamDecorator } from '@nestjs/common';
+import { GqlExecutionContext } from '@nestjs/graphql';
 import { IS_PUBLIC_KEY } from 'src/_constants';
 
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
+
+export const CurrentUser = createParamDecorator((data: unknown, context: ExecutionContext) => {
+	const ctx = GqlExecutionContext.create(context);
+	return ctx.getContext().req.user;
+});
