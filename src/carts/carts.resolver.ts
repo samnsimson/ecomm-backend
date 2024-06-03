83bfeeee01708111d7ba08afdda1e1668efda7e9
samnsimson <samnishanth01@gmail.com>
2024-06-03 18:49:15 -0400

    carts service cleaup

--- src/carts/carts.resolver.ts
+++ src/carts/carts.resolver.ts
@@ -1,76 +1,19 @@
-import { Resolver, Query, Mutation, Args, Int, ResolveField, Parent } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
 import { CartsService } from './carts.service';
 import { Cart } from './entities/cart.entity';
-import { CreateCartInput } from './dto/create-cart.input';
-// import { UpdateCartInput } from './dto/update-cart.input';
 import { CurrentUser, Public } from 'src/_decorator';
 import { CurrentUserType } from 'src/_libs/types';
-import { BadRequestException, UnauthorizedException } from '@nestjs/common';
-import { ProductsService } from 'src/products/products.service';
 import { ProductInfo } from './dto/cart-products.input';
-import { In } from 'typeorm';
 import { CartProductOutput } from './dto/cart-products.output';
 
 @Resolver(() => Cart)
 export class CartsResolver {
-	constructor(
-		private readonly cartsService: CartsService,
-		private readonly productService: ProductsService,
-	) {}
+	constructor(private readonly cartsService: CartsService) {}
 
 	@Public()
-	@Query(() => CartProductOutput, { name: 'cartProducts' })
-	async getCartProducts(@Args('input', { type: () => [ProductInfo] }) products: Array<ProductInfo>, @CurrentUser() user: CurrentUserType) {
-		const pdts = await this.productService.findAll({ where: { id: In(products.map((x) => x.id)) } });
-		const output: CartProductOutput['products'] = [];
-		for (const product of products) {
-			const pdt = pdts.find((x) => x.id === product.id);
-			if (!pdt) continue;
-			const { title, slug, salePrice, retailPrice } = pdt;
-			output.push({ ...product, title, slug, salePrice, retailPrice, total: salePrice * product.quantity });
-		}
+	@Query(() => CartProductOutput, { name: 'cart' })
+	async getCart(@Args('input', { type: () => [ProductInfo] }) products: Array<ProductInfo>, @CurrentUser() user: CurrentUserType) {
+		const output = user ? await this.cartsService.getCartForUser(user.id, products) : await this.cartsService.getCartForGuest(products);
 		return { total: output.reduce((a, b) => a + b.total, 0), products: output };
-	}
-
-	@Mutation(() => Cart)
-	async createCart(@Args('createCartInput') createCartInput: CreateCartInput, @CurrentUser() currentUser: CurrentUserType) {
-		if (!currentUser) throw new UnauthorizedException('Unauthorized');
-		return await this.cartsService.create({ ...createCartInput, userId: currentUser.id });
-	}
-
-	@Query(() => [Cart], { name: 'carts' })
-	findAll() {
-		return this.cartsService.findAll({});
-	}
-
-	@Query(() => Cart, { name: 'cart' })
-	findOne(@Args('cartId', { nullable: true }) cartId?: string, @Args('userId', { nullable: true }) userId?: string) {
-		if (!cartId && !userId) throw new BadRequestException('Either cartId or userId must be provider');
-		return this.cartsService.findOne({ where: [{ id: cartId }, { user: { id: userId } }] });
-	}
-
-	// @Mutation(() => Cart)
-	// updateCart(@Args('updateCartInput') updateCartInput: UpdateCartInput) {
-	// 	return this.cartsService.update(updateCartInput.id, updateCartInput);
-	// }
-
-	@Mutation(() => Cart)
-	removeCart(@Args('id', { type: () => Int }) id: number) {
-		return this.cartsService.remove(id);
-	}
-
-	@ResolveField(() => Int, { name: 'subTotal' })
-	calculateSubTotal(@Parent() cart: Cart) {
-		const subTotal = cart.items.reduce((a, b) => a + b.total, 0);
-		return subTotal;
-	}
-	@ResolveField(() => Int, { name: 'discount' })
-	calculateDiscount(@Parent() cart: Cart) {
-		return 0;
-	}
-	@ResolveField(() => Int, { name: 'total' })
-	calculateTotal(@Parent() cart: Cart) {
-		const total = this.calculateSubTotal(cart) - this.calculateDiscount(cart);
-		return total;
 	}
 }
--- src/carts/carts.service.ts
+++ src/carts/carts.service.ts
@@ -1,36 +1,73 @@
 import { Injectable } from '@nestjs/common';
-import { CreateCartInput, Item } from './dto/create-cart.input';
-// import { UpdateCartInput } from './dto/update-cart.input';
 import { InjectEntityManager, InjectRepository } from '@nestjs/typeorm';
 import { Cart } from './entities/cart.entity';
-import { EntityManager, FindManyOptions, FindOneOptions, Repository } from 'typeorm';
+import { EntityManager, FindManyOptions, FindOneOptions, In, Repository } from 'typeorm';
 import { CartItem } from './entities/cart-item.entity';
+import { ProductInfo } from './dto/cart-products.input';
+import { CartProductOutput } from './dto/cart-products.output';
+import { ProductsService } from 'src/products/products.service';
+import { Product } from 'src/products/entities/product.entity';
 
 @Injectable()
 export class CartsService {
 	constructor(
 		@InjectEntityManager() private readonly entityManager: EntityManager,
 		@InjectRepository(Cart) private readonly cart: Repository<Cart>,
-		@InjectRepository(CartItem) private readonly cartItem: Repository<CartItem>,
+		private readonly productService: ProductsService,
 	) {}
 
-	private async getCartOrCreate(entityManager: EntityManager, userId: string) {
-		const cart = await entityManager.findOne(Cart, { where: { user: { id: userId } }, relations: { user: true, items: true } });
-		if (!cart) return entityManager.save(Cart, entityManager.create(Cart, { user: { id: userId } }));
-		return cart;
+	async getCartForGuest(input: Array<ProductInfo>): Promise<CartProductOutput['products']> {
+		const data = await this.productService.findAll({ where: { id: In(input.map((x) => x.id)) } });
+		const output: CartProductOutput['products'] = [];
+		for (const product of input) {
+			const pdt = data.find((x) => x.id === product.id);
+			if (!pdt) continue;
+			const { title, slug, salePrice, retailPrice } = pdt;
+			const total = salePrice * product.quantity;
+			output.push({ ...product, title, slug, salePrice, retailPrice, total });
+		}
+		return output;
 	}
 
-	private async createCartItem(em: EntityManager, cartId: string, items: Array<Item>) {
-		const cartItems = items.map(({ id, quantity, price }) => em.create(CartItem, { cart: { id: cartId }, product: { id }, quantity, price }));
-		return await em.save(CartItem, cartItems);
-	}
-
-	async create(createCartInput: CreateCartInput & { userId: string }) {
-		return await this.entityManager.transaction(async (entityManager) => {
-			const cart = await this.getCartOrCreate(entityManager, createCartInput.userId);
-			await this.createCartItem(entityManager, cart.id, createCartInput.items);
-			return await entityManager.findOne(Cart, { where: { id: cart.id } });
+	async getCartForUser(id: string, input: Array<ProductInfo>): Promise<CartProductOutput['products']> {
+		return this.entityManager.transaction(async (em) => {
+			const products = await em.find(Product, { where: { id: In(input.map((x) => x.id)) } });
+			const cart = await this.getCartOrCreate(em, id);
+			const items = cart.items ?? (await em.find(CartItem, { where: { cart: { id: cart.id } } }));
+			const productsMap = new Map(products.map((product) => [product.id, product]));
+			const itemsMap = new Map(items.map((item) => [item.product.id, item]));
+			const { output, newItems, updatePromises } = this.createOrUpdatecart(input, productsMap, itemsMap, em, cart);
+			newItems.length && (await em.save(CartItem, newItems));
+			updatePromises.length && (await Promise.all(updatePromises));
+			return output;
 		});
+	}
+
+	private createOrUpdatecart(input: ProductInfo[], productsMap: Map<string, Product>, itemsMap: Map<string, CartItem>, em: EntityManager, cart: Cart) {
+		const output = [];
+		const newItems = [];
+		const updatePromises = [];
+
+		for (const pdt of input) {
+			const { title, slug, salePrice, retailPrice } = productsMap.get(pdt.id);
+			const total = pdt.quantity * salePrice;
+			output.push({ ...pdt, title, slug, salePrice, retailPrice, price: salePrice, total });
+			if (itemsMap.has(pdt.id)) {
+				const item = itemsMap.get(pdt.id);
+				updatePromises.push(em.update(CartItem, item.id, { quantity: pdt.quantity, price: total }));
+			} else {
+				const cartData = { cart: { id: cart.id } };
+				const productData = { product: { id: pdt.id } };
+				const rest = { quantity: pdt.quantity, price: salePrice, total };
+				newItems.push(em.create(CartItem, { ...cartData, ...productData, ...rest }));
+			}
+		}
+		return { output, newItems, updatePromises };
+	}
+
+	private async getCartOrCreate(em: EntityManager, id: string) {
+		const cart = await em.findOne(Cart, { where: { user: { id } } });
+		return cart ?? (await em.save(Cart, em.create(Cart, { user: { id } })));
 	}
 
 	async findAll(args: FindManyOptions<Cart>) {
@@ -39,13 +76,5 @@ export class CartsService {
 
 	async findOne(args: FindOneOptions<Cart>) {
 		return await this.cart.findOne(args);
-	}
-
-	// update(id: number, updateCartInput: UpdateCartInput) {
-	// 	return `This action updates a #${id} cart`;
-	// }
-
-	remove(id: number) {
-		return `This action removes a #${id} cart`;
 	}
 }
